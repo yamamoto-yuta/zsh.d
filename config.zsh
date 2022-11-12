a4219b7ce293331a75551f3a7072b0d1b36624a0
yamamoto-yuta <automatuX78@gmail.com>
2022-11-12 15:49:35 +0900

    🚮ghコマンドを使ったaliasを無効化

--- config.zsh
+++ config.zsh
@@ -16,4 +16,4 @@ export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 
 # GitHub CLI
-eval "$(gh completion -s zsh)"
+#eval "$(gh completion -s zsh)"
