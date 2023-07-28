6353955ae5cef2f842ec9722e10cc60311f3a6c9
yamamoto-yuta <automatuX78@gmail.com>
2023-07-29 02:10:12 +0900

    [FIX] #6

--- alias.d/fa.zsh
+++ alias.d/fa.zsh
@@ -3,6 +3,10 @@
 alias fa='function_fa'
 function function_fa() {
   alias >| ~/.alias_list
-  local selected_alias=$(alias | sed -e 's/=.*$//' | fzf --prompt="alias > " --preview-window=60%:wrap --preview "~/.zsh.d/scripts/show_source.zsh {} | batcat -l sh --color=always --style=grid")
+  if [[ $(uname -a) == *"WSL"* ]]; then
+    local selected_alias=$(alias | sed -e 's/=.*$//' | fzf --prompt="alias > " --preview-window=60%:wrap --preview "~/.zsh.d/scripts/show_source.zsh {} | batcat -l sh --color=always --style=grid")
+  else
+    local selected_alias=$(alias | sed -e 's/=.*$//' | fzf --prompt="alias > " --preview-window=60%:wrap --preview "~/.zsh.d/scripts/show_source.zsh {} | bat -l sh --color=always --style=grid")
+  fi
   print -z $selected_alias
 }
